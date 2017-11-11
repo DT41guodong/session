d96a1a68791dd206f48814a7e7ded266ab20d32a
DT41guodong <13627265857@163.com>
2017-11-11 22:11:03 +0800

    tijiao

---
+++ src/main/java/cn/java/demo/Demo.java
@@ -0,0 +1,11 @@
+package cn.java.demo;
+
+public class Demo {
+    public void show() {
+        System.out.println("abc");
+    }
+
+    public void show(int a) {
+        System.out.println(a);
+    }
+}
